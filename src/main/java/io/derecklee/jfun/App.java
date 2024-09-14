2b67dbe808ad051e16b424ef68614e0694084dd1
李铭杰 <dereckleemj@gmail.com>
2024-09-14 13:13:43 +0800

    Started

---
+++ src/main/java/io/derecklee/jfun/App.java
@@ -0,0 +1,11 @@
+package io.derecklee.jfun;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App {
+
+  public static void main(String[] args) {
+    log.info(">> App is running...");
+  }
+}
